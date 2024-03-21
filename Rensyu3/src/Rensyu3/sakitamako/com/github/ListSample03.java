79383b95e558b8d1ad567e093256a67d7effe769
sakitamako <sa.919.1025.ki@gmail.com>
2024-03-21 22:31:57 +0900

    インデントの編集

--- Rensyu3/src/Rensyu3/sakitamako/com/github/ListSample03.java
+++ Rensyu3/src/Rensyu3/sakitamako/com/github/ListSample03.java
@@ -6,26 +6,26 @@ import java.util.List;
 public class ListSample03 {
 	
 	public static void main(String[] args) {
-	        List<String> list = new ArrayList<String>();
-	        
-	        //値の記憶は　add メソッドを利用
-	        list.add("1");
-	        list.add("2");
-	        list.add("3");
-	        list.add("4");
-	        list.add("5");
-	        
-	        //値の取得
-	        for (int i = 0; i < list.size(); i++) {
-	            //値の取得は get メソッドを利用
-	            System.out.println(list.get(i));
-	        }
-
-	        //拡張 for 文を利用するともっと簡単
-	        for (String s : list) {
-	            System.out.println(s); 
+		List<String> list = new ArrayList<String>();
+		
+		//値の記憶は　add メソッドを利用
+	    list.add("1");
+	    list.add("2");
+	    list.add("3");
+	    list.add("4");
+	    list.add("5");
+	    
+	    //値の取得
+	    for (int i = 0; i < list.size(); i++) {
+	    //値の取得は get メソッドを利用
+	    System.out.println(list.get(i));
 	    }
-	        
-	}
 
+	    //拡張 for 文を利用するともっと簡単
+	    for (String s : list) {
+	    	System.out.println(s);
+	    }
+	    
+	}
+	
 }
